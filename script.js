f8c781fff6b685dfd560241027127338cc491aac
Tanishq Gandhi <tangandhi19@gmail.com>
2022-01-04 15:11:43 +0530

    initial changes

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("mynavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
