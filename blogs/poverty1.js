977142d3d09b17dad67d5fe63b8e63e6e2b61968
Tanishq Gandhi <tangandhi19@gmail.com>
2022-02-04 23:25:40 +0530

    about us updation

--- blogs/poverty1.js
+++ blogs/poverty1.js
@@ -1,7 +1,24 @@
 function EarlyMethods() {
     var divElement = document.getElementById('viz1643692883226');
     var vizElement = divElement.getElementsByTagName('object')[0];
-    if (divElement.offsetWidth > 800) { vizElement.style.minWidth = '420px'; vizElement.style.maxWidth = '650px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '387px'; vizElement.style.maxHeight = '507px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else if (divElement.offsetWidth > 500) { vizElement.style.minWidth = '420px'; vizElement.style.maxWidth = '650px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '387px'; vizElement.style.maxHeight = '507px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else { vizElement.style.width = '100%'; vizElement.style.height = '727px'; }
+    if (divElement.offsetWidth > 800) { 
+        vizElement.style.minWidth = '420px'; 
+        vizElement.style.maxWidth = '650px'; 
+        vizElement.style.width = '100%'; 
+        vizElement.style.minHeight = '387px'; 
+        vizElement.style.maxHeight = '507px'; 
+        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; 
+    } else if (divElement.offsetWidth > 500) { 
+        vizElement.style.minWidth = '420px'; 
+        izElement.style.maxWidth = '650px'; 
+        vizElement.style.width = '100%'; 
+        vizElement.style.minHeight = '387px'; 
+        vizElement.style.maxHeight = '507px'; 
+        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; 
+    } else { 
+        vizElement.style.width = '100%';
+        vizElement.style.height = '727px'; 
+    }
     var scriptElement = document.createElement('script');
     scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
     vizElement.parentNode.insertBefore(scriptElement, vizElement);
