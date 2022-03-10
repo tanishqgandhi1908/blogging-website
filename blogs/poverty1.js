25b9dbdeee3c2434aae3268cd8dc63973a996cbd
Tanishq Gandhi <tangandhi19@gmail.com>
2022-03-11 00:44:27 +0530

    Third blog

--- blogs/poverty1.js
+++ blogs/poverty1.js
@@ -42,18 +42,18 @@ function MPIMobile() {
 
 function Statewise() {
 
-    // var divElement = document.getElementById('viz1643693014281');
-    // var vizElement = divElement.getElementsByTagName('object')[0];
-    // if (divElement.offsetWidth > 800) { vizElement.style.minWidth = '420px'; vizElement.style.maxWidth = '650px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '587px'; vizElement.style.maxHeight = '887px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else if (divElement.offsetWidth > 500) { vizElement.style.minWidth = '420px'; vizElement.style.maxWidth = '650px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '587px'; vizElement.style.maxHeight = '887px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else { vizElement.style.width = '100%'; vizElement.style.height = '3577px'; }
-    // var scriptElement = document.createElement('script');
-    // scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    // vizElement.parentNode.insertBefore(scriptElement, vizElement);
-    var divElement = document.getElementById('viz1646394672382'); 
-    var vizElement = divElement.getElementsByTagName('object')[0]; 
-    if (divElement.offsetWidth > 800) { vizElement.style.width = '650px'; vizElement.style.height = '627px'; } else if (divElement.offsetWidth > 500) { vizElement.style.width = '650px'; vizElement.style.height = '627px'; } else { vizElement.style.width = '100%'; vizElement.style.height = '3577px'; } 
-    var scriptElement = document.createElement('script'); 
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; 
+    var divElement = document.getElementById('viz1646923265553');                    
+    var vizElement = divElement.getElementsByTagName('object')[0];                    
+    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='600px';vizElement.style.height='627px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='600px';vizElement.style.height='627px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     
+    var scriptElement = document.createElement('script');                    
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
     vizElement.parentNode.insertBefore(scriptElement, vizElement);
+    // var divElement = document.getElementById('viz1646394672382'); 
+    // var vizElement = divElement.getElementsByTagName('object')[0]; 
+    // if (divElement.offsetWidth > 800) { vizElement.style.width = '650px'; vizElement.style.height = '627px'; } else if (divElement.offsetWidth > 500) { vizElement.style.width = '650px'; vizElement.style.height = '627px'; } else { vizElement.style.width = '100%'; vizElement.style.height = '3577px'; } 
+    // var scriptElement = document.createElement('script'); 
+    // scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js'; 
+    // vizElement.parentNode.insertBefore(scriptElement, vizElement);
 
 }
 
---
+++ blogs/women.js
@@ -0,0 +1,50 @@
+function yearly() {
+    var divElement = document.getElementById('viz1646923003721');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    if (divElement.offsetWidth > 800) { vizElement.style.width = '850px'; vizElement.style.height = '627px'; } else if (divElement.offsetWidth > 500) { vizElement.style.width = '850px'; vizElement.style.height = '627px'; } else { vizElement.style.width = '100%'; vizElement.style.height = (divElement.offsetWidth * 1.77) + 'px'; }
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
+}
+
+function rural_urban() {
+
+    var divElement = document.getElementById('viz1646923038239');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    if (divElement.offsetWidth > 800) { vizElement.style.minWidth = '600px'; vizElement.style.maxWidth = '800px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '627px'; vizElement.style.maxHeight = '827px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else if (divElement.offsetWidth > 500) { vizElement.style.minWidth = '600px'; vizElement.style.maxWidth = '800px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '627px'; vizElement.style.maxHeight = '827px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else { vizElement.style.width = '100%'; vizElement.style.height = (divElement.offsetWidth * 1.77) + 'px'; }
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
+}
+
+function state_wise() {
+    var divElement = document.getElementById('viz1646923077259');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    if (divElement.offsetWidth > 800) { vizElement.style.minWidth = '600px'; vizElement.style.maxWidth = '750px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '627px'; vizElement.style.maxHeight = '777px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else if (divElement.offsetWidth > 500) { vizElement.style.minWidth = '600px'; vizElement.style.maxWidth = '750px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '627px'; vizElement.style.maxHeight = '777px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else { vizElement.style.width = '100%'; vizElement.style.height = (divElement.offsetWidth * 1.77) + 'px'; }
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
+}
+
+function global() {
+    var divElement = document.getElementById('viz1646923110716');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    if (divElement.offsetWidth > 800) { vizElement.style.minWidth = '600px'; vizElement.style.maxWidth = '800px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '527px'; vizElement.style.maxHeight = '677px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else if (divElement.offsetWidth > 500) { vizElement.style.minWidth = '600px'; vizElement.style.maxWidth = '800px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '527px'; vizElement.style.maxHeight = '677px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else { vizElement.style.width = '100%'; vizElement.style.height = '1827px'; }
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
+}
+
+function global_mobile() {
+    var divElement = document.getElementById('viz1646923201198');
+    var vizElement = divElement.getElementsByTagName('object')[0];
+    if (divElement.offsetWidth > 800) { vizElement.style.minWidth = '600px'; vizElement.style.maxWidth = '800px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '527px'; vizElement.style.maxHeight = '677px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else if (divElement.offsetWidth > 500) { vizElement.style.minWidth = '600px'; vizElement.style.maxWidth = '800px'; vizElement.style.width = '100%'; vizElement.style.minHeight = '527px'; vizElement.style.maxHeight = '677px'; vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px'; } else { vizElement.style.width = '100%'; vizElement.style.height = '427px'; }
+    var scriptElement = document.createElement('script');
+    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+
+}
